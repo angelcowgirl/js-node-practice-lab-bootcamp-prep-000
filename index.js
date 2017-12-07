383601a8ae53294390d04291ae741fa73320c18e
tami winn <uthippy@gmail.com>
2017-12-07 00:05:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Tami"
-var height = "74"
+var height = "510"
 
 console.log("Name:")
 console.log(name)
 
-console.log("Height:")
+console.log("Height+1:")
 console.log(height)
